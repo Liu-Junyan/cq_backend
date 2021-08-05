96d0082bc7e9d6c5aa2fe99317fda06080ffed95
Liu Junyan <hysw2009@gmail.com>
2021-08-05 20:50:44 +0800

    Use object instead of companion object in classes.

--- src/main/kotlin/LiveList.kt
+++ src/main/kotlin/LiveList.kt
@@ -7,55 +7,54 @@ import java.time.temporal.ChronoUnit
 
 private val logger = KotlinLogging.logger {}
 
-class LiveList() {
-    companion object {
-        private var liveList: MutableList<Live> = mutableListOf<Live>()
-        private var lastUpdateTime: LocalDateTime? = null
-        private var debugMode: Boolean = false
 
-        fun get(): MutableList<Live> {
-            checkUpdate()
-            return liveList
-        }
-        fun activateDebugMode(debugMode: Boolean) {
-            this.debugMode = debugMode
-        }
-        private fun checkUpdate() {
-            val initialTime = LocalDateTime.now()
-            if (lastUpdateTime == null) {
-                update(initialTime)
+object LiveList {
+    private var liveList: MutableList<Live> = mutableListOf<Live>()
+    private var lastUpdateTime: LocalDateTime? = null
+    private var debugMode: Boolean = false
+
+    fun get(): MutableList<Live> {
+        checkUpdate()
+        return liveList
+    }
+    fun activateDebugMode(debugMode: Boolean) {
+        this.debugMode = debugMode
+    }
+    private fun checkUpdate() {
+        val initialTime = LocalDateTime.now()
+        if (lastUpdateTime == null) {
+            update(initialTime)
+        } else {
+            logger.debug { "Check for update at $initialTime" }
+            val interval = if (debugMode) {
+                ChronoUnit.SECONDS.between(lastUpdateTime, initialTime)
             } else {
-                logger.debug { "Check for update at $initialTime" }
-                val interval = if (debugMode) {
-                    ChronoUnit.SECONDS.between(lastUpdateTime, initialTime)
-                } else {
-                    ChronoUnit.MINUTES.between(lastUpdateTime, initialTime)
-                }
-                if (interval >= 29) {
-                    update(initialTime)
-                }
+                ChronoUnit.MINUTES.between(lastUpdateTime, initialTime)
+            }
+            if (interval >= 29) {
+                update(initialTime)
             }
         }
+    }
 
-        private fun update(initialTime: LocalDateTime) {
-            lastUpdateTime = initialTime
-            val response = getResponse()
-            liveList.clear()
-            parseJson(response)
-            logger.info { "LiveList updated at $initialTime" }
-        }
-        private fun getResponse(): String {
-            val url = "https://api.holotools.app/v1/live?hide_channel_desc=1&lookback_hours=0"
-            return URL(url).readText()
-        }
-        private fun parseJson(text: String) {
-            val liveJSONList = Json {ignoreUnknownKeys = true} .decodeFromString<LiveJSONList>(text)
-            for (live in liveJSONList.live) {
-                if (live.channel.subscriber_count > 50_0000) {
-                    liveList.add(live)
-                }
+    private fun update(initialTime: LocalDateTime) {
+        lastUpdateTime = initialTime
+        val response = getResponse()
+        liveList.clear()
+        parseJson(response)
+        logger.info { "LiveList updated at $initialTime" }
+    }
+    private fun getResponse(): String {
+        val url = "https://api.holotools.app/v1/live?hide_channel_desc=1&lookback_hours=0"
+        return URL(url).readText()
+    }
+    private fun parseJson(text: String) {
+        val liveJSONList = Json {ignoreUnknownKeys = true} .decodeFromString<LiveJSONList>(text)
+        for (live in liveJSONList.live) {
+            if (live.channel.subscriber_count > 50_0000) {
+                liveList.add(live)
             }
-            liveList.sortBy { it.channel.name }
         }
+        liveList.sortBy { it.channel.name }
     }
 }
--- src/main/kotlin/SessionPool.kt
+++ src/main/kotlin/SessionPool.kt
@@ -1,23 +1,21 @@
-class SessionPool {
-    companion object {
-        private val sessions: MutableSet<Session> = mutableSetOf<Session>()
+object SessionPool {
+    private val sessions: MutableSet<Session> = mutableSetOf<Session>()
 
-        fun load(recipientsConfig: RecipientsConfig) {
-            for (recipient in recipientsConfig.recipients) {
-                sessions.add(Session(recipient))
+    fun load(recipientsConfig: RecipientsConfig) {
+        for (recipient in recipientsConfig.recipients) {
+            sessions.add(Session(recipient))
+        }
+    }
+
+    fun periodicFire(min: Int) {
+        for (session in sessions) {
+            if (session.shouldSendAt(min)) {
+                session.send()
             }
         }
+    }
 
-        fun periodicFire(min: Int) {
-            for (session in sessions) {
-                if (session.shouldSendAt(min)) {
-                    session.send()
-                }
-            }
-        }
-
-        fun getSession(id: Long, type: RecipientType): Session? {
-            return(sessions.find { it.recipient.id == id && it.recipient.type == type })
-        }
+    fun getSession(id: Long, type: RecipientType): Session? {
+        return(sessions.find { it.recipient.id == id && it.recipient.type == type })
     }
 }
